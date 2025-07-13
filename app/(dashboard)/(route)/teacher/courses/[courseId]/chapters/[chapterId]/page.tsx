562f8189968b900338572be18a700b282d213c30
Omkrishna-git <omdpatil07@gmail.com>
2025-07-13 12:43:22 +0530

    chapter descprition form also added rich editor & preview...

--- app/(dashboard)/(route)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(route)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -8,7 +8,7 @@ import { IconBadge } from "@/components/icon-badge";
 // import { Banner } from "@/components/banner";
 
 import { ChapterTitleForm } from "./_components/chapter-title-form";
-// import { ChapterDescriptionForm } from "./_components/chapter-description-form";
+import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 // import { ChapterAccessForm } from "./_components/chapter-access-form";
 // import { ChapterVideoForm } from "./_components/chapter-video-form";
 // import { ChapterActions } from "./_components/chapter-actions";
@@ -109,13 +109,14 @@ const ChapterIdPage = async ({
                                 chapterId={params.chapterId}
                             />
 
-                            {/* <ChapterDescriptionForm
+                            <ChapterDescriptionForm
                                 initialData={chapter}
                                 courseId={params.courseId}
                                 chapterId={params.chapterId}
-                            /> */}
+                            />
 
                         </div>
+                        
                         <div>
                             <div className="flex items-center gap-x-2">
                                 <IconBadge icon={Eye} />
